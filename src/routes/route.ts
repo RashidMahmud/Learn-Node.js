4662ac115e2c0bc33f5d1249b9a384592fdd818a
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-10 21:20:26 +0600

    Better file organization

---
+++ src/routes/route.ts
@@ -0,0 +1,20 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { productController } from "../controller/productController";
+
+export const routeHandler = (req:IncomingMessage, res:ServerResponse) => {
+// console.log(req.url)
+// console.log(req.method)
+const url = req.url
+const method = req.method
+if(url === "/" && method === "GET"){
+    // console.log("This is root route")
+    res.writeHead(200, {"content-type": "application/json"})
+    res.end(JSON.stringify({message:"This is root route"}))
+}   else if(url?.startsWith('/products')){
+    productController(req,res);
+}
+    else{
+    res.writeHead(404, {"content-type": "application/json"})
+    res.end(JSON.stringify({message:"Route not found"}))
+}
+}
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
 import { createServer, IncomingMessage, Server } from "http";
+import { routeHandler } from "./routes/route";
 
 const server: Server = createServer((req: IncomingMessage,res)=> {
-// console.log(req.url)
-// console.log(req.method)
-const url = req.url
-const method = req.method
-if(url === "/" && method === "GET"){
-    // console.log("This is root route")
-    res.writeHead(200, {"content-type": "application/json"})
-    res.end(JSON.stringify({message:"This is root route"}))
-}else if(url?.startsWith('/products')){
-    res.writeHead(200, {"content-type": "application/json"})
-    res.end(JSON.stringify({message:"This is products route"}))
+routeHandler(req,res)
 }
-    else{
-    res.writeHead(404, {"content-type": "application/json"})
-    res.end(JSON.stringify({message:"Route not found"}))
-}
-})
+)
 
 server.listen(5000, ()=> {
     console.log("Server is running on port 5000")
