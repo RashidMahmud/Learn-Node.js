d29789ae3314f32679bc88b8c958cdc4aaa60bca
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-11 21:39:21 +0600

    testing localhost

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -6,15 +6,18 @@ export const productController = (req:IncomingMessage, res:ServerResponse) => {
 const method = req.method
  if(url === "/products" && method === "GET"){
 
-    const products = [
-        {
-            id : 1,
-            name : "Product 1",
-        },
-    ]
-    readProduct()
+    // const products = [
+    //     {
+    //         id : 1,
+    //         name : "Product 1",
+    //     },
+    // ]
+    const products =readProduct()
 
     res.writeHead(200, {"content-type": "application/json"})
-    res.end(JSON.stringify({message:"Products retrieved successfully", data:{products}}))
+    res.end(
+        JSON.stringify(
+            {message:"Products retrieved successfully",
+            data:{products}}))
 }
 }
