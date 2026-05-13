042f7a92070c709205fc8839ae5c0153c0e62b05
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-13 15:00:38 +0600

    parseBody completed

--- src/utility/parseBody.ts
+++ src/utility/parseBody.ts
@@ -1,18 +1,17 @@
-import { resolveSoa } from "node:dns";
-import type { IncomingMessage } from "node:http";
+import type { IncomingMessage } from "http";
 
 export const parseBody = (req: IncomingMessage): Promise<any> => {
-        return new Promise((resolve, reject)=>{
-            let body = "";
-            req.on('data', (chunk) => {
-                body += chunk
-            })
-            req.on('end', ()=> {
-                try {
-                    resolve(body)
-                } catch (error) {
-                    reject(error) 
-                }
-            })
-        })
-}
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
